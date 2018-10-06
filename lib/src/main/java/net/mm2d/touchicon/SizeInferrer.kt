7a2c111834c5ea32987938c3dcdda869f697a940
OHMAE Ryosuke <ryo@mm2d.net>
2018-10-06 19:49:18 +0900

    :sparkles: failure as -1

--- lib/src/main/java/net/mm2d/touchicon/SizeInferrer.kt
+++ lib/src/main/java/net/mm2d/touchicon/SizeInferrer.kt
@@ -17,7 +17,7 @@ internal fun inferSizeFromUrl(url: String): Point {
     val fileName = url.substring(url.lastIndexOf('/'))
     val matcher = Pattern.compile("\\d+x\\d+").matcher(fileName)
     if (!matcher.find()) {
-        return Point(0, 0)
+        return Point(-1, -1)
     }
     return inferSizeFromSizes(matcher.group())
 }
@@ -25,7 +25,7 @@ internal fun inferSizeFromUrl(url: String): Point {
 internal fun inferSizeFromSizes(sizes: String): Point {
     val part = sizes.split('x')
     if (part.size == 2) {
-        return Point(part[0].toIntOrNull() ?: 0, part[1].toIntOrNull() ?: 0)
+        return Point(part[0].toIntOrNull() ?: -1, part[1].toIntOrNull() ?: -1)
     }
-    return Point(0, 0)
+    return Point(-1, -1)
 }
