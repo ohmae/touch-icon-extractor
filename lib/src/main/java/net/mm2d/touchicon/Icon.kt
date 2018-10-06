80997491deaeb8c1af95b56da9c19db1a2c1fa13
OHMAE Ryosuke <ryo@mm2d.net>
2018-10-06 19:12:34 +0900

    :recycle: extract function

--- lib/src/main/java/net/mm2d/touchicon/Icon.kt
+++ lib/src/main/java/net/mm2d/touchicon/Icon.kt
@@ -8,7 +8,6 @@
 package net.mm2d.touchicon
 
 import android.graphics.Point
-import java.util.regex.Pattern
 
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
@@ -21,21 +20,4 @@ interface Icon {
     val precomposed: Boolean
     val length: Int
     fun inferSize(): Point
-}
-
-internal fun inferSizeFromUrl(url: String): Point {
-    val fileName = url.substring(url.lastIndexOf('/'))
-    val matcher = Pattern.compile("\\d+x\\d+").matcher(fileName)
-    if (!matcher.find()) {
-        return Point(0, 0)
-    }
-    return inferSizeFromSizes(matcher.group())
-}
-
-internal fun inferSizeFromSizes(sizes: String): Point {
-    val part = sizes.split('x')
-    if (part.size == 2) {
-        return Point(part[0].toIntOrNull() ?: 0, part[1].toIntOrNull() ?: 0)
-    }
-    return Point(0, 0)
 }
---
+++ lib/src/main/java/net/mm2d/touchicon/SizeInferrer.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+package net.mm2d.touchicon
+
+import android.graphics.Point
+import java.util.regex.Pattern
+
+/**
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
+ */
+internal fun inferSizeFromUrl(url: String): Point {
+    val fileName = url.substring(url.lastIndexOf('/'))
+    val matcher = Pattern.compile("\\d+x\\d+").matcher(fileName)
+    if (!matcher.find()) {
+        return Point(0, 0)
+    }
+    return inferSizeFromSizes(matcher.group())
+}
+
+internal fun inferSizeFromSizes(sizes: String): Point {
+    val part = sizes.split('x')
+    if (part.size == 2) {
+        return Point(part[0].toIntOrNull() ?: 0, part[1].toIntOrNull() ?: 0)
+    }
+    return Point(0, 0)
+}
