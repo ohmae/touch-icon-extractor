11d85895a856df252fc4cb1977847ce0401ec87d
OHMAE Ryosuke <ryo@mm2d.net>
2018-10-08 04:42:37 +0900

    :+1: progress bar

--- sample/src/main/java/net/mm2d/touchicon/sample/MainActivity.kt
+++ sample/src/main/java/net/mm2d/touchicon/sample/MainActivity.kt
@@ -8,9 +8,12 @@
 package net.mm2d.touchicon.sample
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,24 +25,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView.settings.javaScriptEnabled = true
-        webView.settings.setSupportZoom(true)
-        webView.settings.builtInZoomControls = true
-        webView.settings.displayZoomControls = false
-        webView.settings.useWideViewPort = true
-        webView.settings.loadWithOverviewMode = true
-        webView.webChromeClient = WebChromeClient()
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://m.yahoo.co.jp/")
-        TouchIconExtractorHolder.extractor.userAgent = webView.settings.userAgentString
+        web_view.settings.javaScriptEnabled = true
+        web_view.settings.setSupportZoom(true)
+        web_view.settings.builtInZoomControls = true
+        web_view.settings.displayZoomControls = false
+        web_view.settings.useWideViewPort = true
+        web_view.settings.loadWithOverviewMode = true
+        web_view.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progress_bar.progress = newProgress
+            }
+        }
+        web_view.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progress_bar.progress = 0
+                progress_bar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progress_bar.visibility = View.INVISIBLE
+            }
+        }
+        web_view.loadUrl("https://m.yahoo.co.jp/")
+        TouchIconExtractorHolder.extractor.userAgent = web_view.settings.userAgentString
         fab.setOnClickListener {
-            IconDialog.show(this, webView.title, webView.url)
+            IconDialog.show(this, web_view.title, web_view.url)
         }
     }
 
     override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
+        if (web_view.canGoBack()) {
+            web_view.goBack()
             return
         }
         super.onBackPressed()
