f5c27abe803c0dcd628dbeaf11439253756076a6
OHMAE Ryosuke <ryo@mm2d.net>
2018-10-08 12:49:51 +0900

    :+1: browsable

--- sample/src/main/java/net/mm2d/touchicon/sample/MainActivity.kt
+++ sample/src/main/java/net/mm2d/touchicon/sample/MainActivity.kt
@@ -8,7 +8,10 @@
 package net.mm2d.touchicon.sample
 
 import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -21,10 +24,41 @@ import kotlinx.android.synthetic.main.activity_main.*
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpWebView()
+        fab.setOnClickListener {
+            IconDialog.show(this, web_view.title, web_view.url)
+        }
+        val url = extractUrlToLoad(intent)
+        if (url.isNotEmpty()) {
+            web_view.loadUrl(url)
+        } else {
+            web_view.loadUrl(DEFAULT_URL)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent ?: return
+        setIntent(intent)
+        val url = extractUrlToLoad(intent)
+        if (url.isNotEmpty()) {
+            web_view.loadUrl(url)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (web_view.canGoBack()) {
+            web_view.goBack()
+            return
+        }
+        super.onBackPressed()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setUpWebView() {
         web_view.settings.javaScriptEnabled = true
         web_view.settings.setSupportZoom(true)
         web_view.settings.builtInZoomControls = true
@@ -46,18 +80,31 @@ class MainActivity : AppCompatActivity() {
                 progress_bar.visibility = View.INVISIBLE
             }
         }
-        web_view.loadUrl("https://m.yahoo.co.jp/")
         TouchIconExtractorHolder.extractor.userAgent = web_view.settings.userAgentString
-        fab.setOnClickListener {
-            IconDialog.show(this, web_view.title, web_view.url)
-        }
     }
 
-    override fun onBackPressed() {
-        if (web_view.canGoBack()) {
-            web_view.goBack()
-            return
+    companion object {
+        private const val DEFAULT_URL = "https://m.yahoo.co.jp/"
+        private const val YAHOO_SEARCH_URL = "https://search.yahoo.co.jp/search?ei=UTF-8"
+        private const val YAHOO_SEARCH_QUERY_KEY = "p"
+
+        private fun extractUrlToLoad(intent: Intent): String {
+            return when (intent.action) {
+                Intent.ACTION_VIEW ->
+                    intent.data?.toString() ?: ""
+                Intent.ACTION_SEARCH, Intent.ACTION_WEB_SEARCH ->
+                    makeSearchUrl(intent.getStringExtra(SearchManager.QUERY) ?: "")
+                else ->
+                    ""
+            }
         }
-        super.onBackPressed()
+
+        private fun makeSearchUrl(query: String): String {
+            return Uri.parse(YAHOO_SEARCH_URL)
+                    .buildUpon()
+                    .appendQueryParameter(YAHOO_SEARCH_QUERY_KEY, query)
+                    .build()
+                    .toString()
+        }
     }
 }
