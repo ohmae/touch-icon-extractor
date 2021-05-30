949cc8a71d6b3947dd3c2bb0d3a65df20fab23f5
OHMAE Ryosuke <ryo@mm2d.net>
2021-05-30 17:51:56 +0900

    🆙kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.5.0"))
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30")
+        classpath(kotlin("gradle-plugin", version = "1.5.10"))
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
 
         classpath("com.github.ben-manes:gradle-versions-plugin:0.38.0")
     }
