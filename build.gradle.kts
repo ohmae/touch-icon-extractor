94ddd8b4d4484fd9a30d10a449a7648159dade61
OHMAE Ryosuke <ryo@mm2d.net>
2021-11-22 11:17:58 +0900

    👍jacocoMergeReport

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,25 +25,15 @@ jacoco {
     toolVersion = "0.8.7"
 }
 
-tasks.create("jacocoMerge", JacocoMerge::class) {
-    group = "verification"
-    gradle.afterProject {
-        if (rootProject != this && plugins.hasPlugin("jacoco")) {
-            executionData("${buildDir}/jacoco/test.exec")
-        }
-    }
-}
-
 tasks.create("jacocoMergedReport", JacocoReport::class) {
     group = "verification"
-    dependsOn("jacocoMerge")
     gradle.afterProject {
         if (rootProject != this && plugins.hasPlugin("jacoco")) {
             sourceDirectories.from += "${projectDir}/src/main/java"
             classDirectories.from.addAll(fileTree("${buildDir}/classes/kotlin/main"))
+            executionData(tasks.withType<Test>())
         }
     }
-    executionData(tasks.named<JacocoMerge>("jacocoMerge").get().destinationFile)
     reports {
         xml.required.set(true)
         html.required.set(true)
