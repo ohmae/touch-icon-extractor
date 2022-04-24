95e2584a5f1a1753683fcb680b4201540f9cbe58
OHMAE Ryosuke <ryo@mm2d.net>
2022-04-24 12:02:08 +0900

    🎉release 0.9.4

--- buildSrc/src/main/java/build/ProjectProperties.kt
+++ buildSrc/src/main/java/build/ProjectProperties.kt
@@ -9,7 +9,7 @@ object ProjectProperties {
 
     private const val versionMajor: Int = 0
     private const val versionMinor: Int = 9
-    private const val versionPatch: Int = 3
+    private const val versionPatch: Int = 4
     const val versionName: String = "$versionMajor.$versionMinor.$versionPatch"
 
     object Url {
